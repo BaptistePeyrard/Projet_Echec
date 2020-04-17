351d9a2642bd4f46946d223c5cb7ef1b3e05c86a
BaptisteP <babate52@hotmail.fr>
2020-04-17 13:52:44 +0100

    test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main()
     char pseudo[50];
     cout << "Quel est votre pseudo ?" << endl;
     cin >> pseudo;
-    cout << "Salut " << pseudo << endl;
-    x = "magrossebite";
+    cout << "Salut petit con de " << pseudo << endl;
     return 0;
 }
