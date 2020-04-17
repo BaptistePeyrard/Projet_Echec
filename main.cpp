ac72274439ac58edde9816aba64c72fc8fe7a759
BaptisteP <babate52@hotmail.fr>
2020-04-17 13:42:13 +0100

    test Github

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char pseudo[50];
+    cout << "Quel est votre pseudo ?" << endl;
+    cin >> pseudo;
+    cout << "Salut " << pseudo << endl;
+    return 0;
+}
